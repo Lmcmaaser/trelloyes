300373bdfda5d3f14bc6f911aebb4e7d07be1a80
Lmcmaaser <lmcmaaser@gmail.com>
2019-12-31 15:43:40 -0700

    update snapshot test, not working

--- src/Card.test.js
+++ src/Card.test.js
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //make the Card component available
 import Card from './Card';
+//add the snapshot tests
+import renderer from 'react-test-renderer';
 
 describe('Cards component', () => {
     //smoke test, ensures that the component renders in the first place
@@ -11,15 +13,17 @@ describe('Cards component', () => {
         // first create a DOM element to render the component into
         const div = document.createElement('div');
         //render the component, this is the actual test, if something is wrong it will fail here
-        ReactDOM.render(<App />, div);
+        ReactDOM.render(<Card />, div);
         //clean up code
         ReactDOM.unmountComponentAtNode(div);
     });
-}
 
-
-/*it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Card />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});*/
+    //Render the component and create a human readable JSON file
+    //Compare the rendered component to a saved version of the component
+    it('renders the UI as expected', () => {
+        const tree = renderer
+          .create(<button type='button'> delete</button>) 
+          .toJSON();
+        expect(tree).toMatchSnapshot();  
+    });
+});
